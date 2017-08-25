58ecd93d1ebc9d8dc41f9a57f3bf7d42496c0c68
cmigz <cmignacca@gmail.com>
2017-08-25 13:56:25 -0400

    CREATED: hidden class MODIFIED: onSignUpSucces
    assets/styles/formatting.scss:
    - Created .hidden class for removing/adding form fields when needed
    assets/api/ui.js:
    - Added jQuery handler to remove form field on sign in success

--- assets/api/ui.js
+++ assets/api/ui.js
@@ -5,7 +5,9 @@ const app = require('../scripts/app.js')
 const onSignUpSuccess = (data) => {
   app.user = data.user
   console.log('Sign Up Working')
+  $('#sign-up').addClass('hidden')
 }
+// TODO Create function for SignUp Failure
 
 module.exports = {
   onSignUpSuccess
