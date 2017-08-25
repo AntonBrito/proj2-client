9cafc3702aff8a379a7649ce8da9d586faf3174f
cmigz <cmignacca@gmail.com>
2017-08-25 14:36:36 -0400

    CREATED: Sign-out Button
    index.html:
    - Added button, initially hidden
    assets/api/ui.js:
    - Modified onSignInSucess to hide sign in field upon success and
      remove hidden class from sign out button

--- assets/api/ui.js
+++ assets/api/ui.js
@@ -8,12 +8,17 @@ const onSignUpSuccess = (data) => {
   $('#sign-up').addClass('hidden')
   $('#sign-in').removeClass('hidden')
 }
+
 // TODO Create function for SignUp Failure
 
 const onSignInSuccess = (data) => {
   app.user = data.user
   console.log('Sign in Working')
+  $('#sign-in').addClass('hidden')
+  $('#sign-out').removeClass('hidden')
 }
+
+// TODO Create function for SignIn failure
 
 module.exports = {
   onSignUpSuccess,
